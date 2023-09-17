224ad73f5f6517e18ad20ca9553e152ccfcf24dd
woniu336 <310478073@qq.com>
2023-09-17 20:24:47 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-# 安装 Docker
-curl -fsSL https://get.docker.com | sh
+# 检查是否已经安装 Docker
+if command -v docker &>/dev/null; then
+    echo "Docker 已经安装。"
+else
+    # 安装 Docker
+    curl -fsSL https://get.docker.com | sh
+    echo "Docker 安装完成。"
+fi
 
-# 安装 Docker Compose
-curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# 检查是否已经安装 Docker Compose
+if command -v docker-compose &>/dev/null; then
+    echo "Docker Compose 已经安装。"
+else
+    # 安装 Docker Compose
+    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    echo "Docker Compose 安装完成。"
+fi
 
-# 检查 Docker 和 Docker Compose 版本
+# 显示 Docker 和 Docker Compose 版本
 docker --version
 docker-compose --version
