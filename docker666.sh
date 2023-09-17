d341f0a2e64664d4bc1ad97a446f12317461d317
woniu336 <310478073@qq.com>
2023-09-17 23:23:51 +0800

    Update docker666.sh

--- docker666.sh
+++ docker666.sh
@@ -27,9 +27,8 @@ confirm_action() {
   read -p "是否继续安装？ (y/n): " answer
   if [ "$answer" != "y" ]; then
     echo "操作已取消."
-    return 1
+    exit 1  # 修复：在取消操作时退出脚本
   fi
-  return 0
 }
 
 # Menu for user to select options
@@ -78,21 +77,21 @@ while true; do
       ;;
     4)
       # 安装 Portainer-ce
-	  if check_docker; then
-      echo "在安装 docker可视化面板前，请确保端口号: 9000 "
-      confirm_action
-      sudo docker run -d --restart=always --name="portainer" -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data 6053537/portainer-ce
-	 else
-      echo "请先安装Docker，然后再安装 docker可视化面板。"
+      if check_docker; then
+        echo "在安装 docker可视化面板前，请确保端口号: 9000 "
+        confirm_action
+        sudo docker run -d --restart=always --name="portainer" -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data 6053537/portainer-ce
+      else
+        echo "请先安装Docker，然后再安装 docker可视化面板。"
       fi
       ;;
     5)
       # 安装 Uptime Kuma
-	  if check_docker; then
-      echo "在安装 Uptime Kuma 前，请确保端口号: 3001 "
-      confirm_action
-      sudo docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1
-	 else
+      if check_docker; then
+        echo "在安装 Uptime Kuma 前，请确保端口号: 3001 "
+        confirm_action
+        sudo docker run -d --restart=always -p 3001:3001 -v uptime-kuma:/app/data --name uptime-kuma louislam/uptime-kuma:1
+      else
         echo "请先安装Docker，然后再安装 Uptime Kuma。"
       fi
       ;;
