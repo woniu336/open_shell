05b9abd73d1f9a57e7920a1558f4e91854dad730
woniu336 <310478073@qq.com>
2023-11-27 19:14:50 +0800

    Update pandoranext.sh

--- pandoranext.sh
+++ pandoranext.sh
@@ -4,50 +4,39 @@
 mkdir -p pandora-next
 cd pandora-next
 
-# 提示用户输入license.jwt的下载地址
-read -p "请输入license.jwt的下载地址: " license_url
+# 提示用户输入License Id
+read -p "请输入License Id: " license_id
 
-# 下载license.jwt文件
-curl -fLO "$license_url"
-
-# 获取license.jwt的值
-license_jwt=$(cat license.jwt)
-
-# 创建docker-compose.yml文件
-cat <<EOF > docker-compose.yml
-version: '3'
-services:
-  pandora-next:
-    image: pengzhile/pandora-next
-    container_name: PandoraNext
-    network_mode: bridge
-    restart: always
-    ports:
-      - "8181:8181"
-    environment:
-      - PANDORA_NEXT_LICENSE=$license_jwt
-    volumes:
-      - ./data:/data
-EOF
-
-# 切换到data目录
+# 创建data目录
 mkdir -p data
-cd data
 
 # 创建config.json文件
-cat <<EOF > config.json
+cat <<EOF > data/config.json
 {
   "bind": "0.0.0.0:8181",
   "timeout": 600,
   "proxy_url": "",
+  "license_id": "$license_id",
   "public_share": false,
   "site_password": "",
+  "setup_password": "",
+  "server_tokens": true,
+  "server_mode": "web",
+  "captcha": {
+    "provider": "",
+    "site_key": "",
+    "site_secret": "",
+    "site_login": false,
+    "setup_login": false,
+    "oai_username": false,
+    "oai_password": false
+  },
   "whitelist": null
 }
 EOF
 
 # 创建tokens.json文件
-cat <<EOF > tokens.json
+cat <<EOF > data/tokens.json
 {
   "test-1": {
     "token": "access token / session token / refresh token / share token",
@@ -67,11 +56,29 @@ cat <<EOF > tokens.json
 }
 EOF
 
-# 回到pandora-next目录
-cd ..
+# 创建docker-compose.yml文件
+cat <<EOF > docker-compose.yml
+version: '3'
+services:
+  pandora-next:
+    image: pengzhile/pandora-next
+    container_name: PandoraNext
+    network_mode: bridge
+    restart: always
+    ports:
+      - "8181:8181"
+    volumes:
+      - ./data:/data
+      - ./sessions:/root/.cache/PandoraNext
+EOF
 
 # 执行容器运行命令
 docker-compose up -d
 
 # 检查容器是否成功启动
-docker-compose ps
+docker-compose ps | grep "PandoraNext" > /dev/null
+if [ $? -eq 0 ]; then
+    echo "PandoraNext 容器已成功启动！"
+else
+    echo "启动失败，请检查日志以获取更多信息。"
+fi
