fff69cdccf6958158958229d9f2712fa70efc3e0
woniu336 <310478073@qq.com>
2024-01-13 23:55:13 +0800

    更新 xiu.sh

--- xiu.sh
+++ xiu.sh
@@ -53,17 +53,9 @@ delete_directories=(
     "/说明文档/"
 )
 
-exclude_directory="/application/extra/"
-
 for dir in "${delete_directories[@]}"; do
-    # 检查是否为排除的目录
-    if [ "$dir" != "$exclude_directory" ]; then
-        rm -rf "$website_directory$dir"
-    else
-        echo "Skipping deletion of $dir"
-    fi
+    rm -rf "$website_directory$dir"
 done
-
 
 # 忽略掉 .user.ini 文件的删除错误
 rm -f "$website_directory/.user.ini" 2>/dev/null
