23be367a14f48d78786350cf5d4fa440df9f80e6
woniu336 <310478073@qq.com>
2024-02-03 01:13:32 +0800

    Docs: 『note』内容更新 2024-02-03 Sat 01:13:32

---
+++ auto_push.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+git add .
+read -p "Please enter commit message: " commitMsg
+if [ -z $commitMsg ];then
+  commitMsg="Docs: 『note』内容更新 $(date +'%F %a %T')"
+fi
+git commit -m "$commitMsg"
+git push -f origin --all  #向存储库推送
---
+++ gzip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 获取用户输入的目录路径
+read -p "请输入目录路径: " directory_path
+
+# 获取目录下的所有css和js文件
+css_js_files=$(find "$directory_path" -type f \( -name "*.css" -o -name "*.js" \))
+
+# 压缩每个文件
+for file in $css_js_files; do
+    gzip -9 -c "$file" > "$file.gz"
+done
+
+# 更新nginx.conf文件
+nginx_conf_path="/www/server/nginx/conf/nginx.conf"
+sed -i '/gzip on;/a\    gzip_static on;' "$nginx_conf_path"
+
+# 重新加载nginx
+nginx -s reload
+
+echo "操作完成。"
