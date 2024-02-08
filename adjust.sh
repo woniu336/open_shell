8b7511a72f95b34d5a01ae5a95a018c56722bab9
woniu336 <310478073@qq.com>
2024-02-08 17:48:16 +0800

    Update adjust.sh

--- adjust.sh
+++ adjust.sh
@@ -2,26 +2,22 @@
 
 # 设置 vm.swappiness 参数为 10
 echo "Setting vm.swappiness to 10"
-echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.conf > /dev/null
+sudo sysctl -w vm.swappiness=10
 
 # 设置 vm.dirty_ratio 参数为 10
 echo "Setting vm.dirty_ratio to 10"
-echo "vm.dirty_ratio=10" | sudo tee -a /etc/sysctl.conf > /dev/null
+sudo sysctl -w vm.dirty_ratio=10
 
 # 设置 vm.dirty_background_ratio 参数为 5
 echo "Setting vm.dirty_background_ratio to 5"
-echo "vm.dirty_background_ratio=5" | sudo tee -a /etc/sysctl.conf > /dev/null
+sudo sysctl -w vm.dirty_background_ratio=5
 
 # 设置 vm.dirty_expire_centisecs 参数为 500
 echo "Setting vm.dirty_expire_centisecs to 500"
-echo "vm.dirty_expire_centisecs=500" | sudo tee -a /etc/sysctl.conf > /dev/null
+sudo sysctl -w vm.dirty_expire_centisecs=500
 
 # 设置 vm.vfs_cache_pressure 参数为 200
 echo "Setting vm.vfs_cache_pressure to 200"
-echo "vm.vfs_cache_pressure=200" | sudo tee -a /etc/sysctl.conf > /dev/null
-
-# 重新加载 sysctl 配置文件以使更改生效
-echo "Reloading sysctl configuration"
-sudo sysctl -p
+sudo sysctl -w vm.vfs_cache_pressure=200
 
 echo "Changes applied successfully."
