58832a9bc3f3785f18843eeb48f825e55d602afe
woniu336 <310478073@qq.com>
2024-02-08 17:53:22 +0800

    Update adjust.sh

--- adjust.sh
+++ adjust.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# 设置 vm.swappiness 参数为 10
-echo "Setting vm.swappiness to 10"
+# 清除之前的设置
+sudo sysctl -w vm.swappiness=0
+sudo sysctl -w vm.dirty_ratio=0
+sudo sysctl -w vm.dirty_background_ratio=0
+sudo sysctl -w vm.dirty_expire_centisecs=0
+sudo sysctl -w vm.vfs_cache_pressure=0
+
+# 设置新的参数值
 sudo sysctl -w vm.swappiness=10
-
-# 设置 vm.dirty_ratio 参数为 10
-echo "Setting vm.dirty_ratio to 10"
 sudo sysctl -w vm.dirty_ratio=10
-
-# 设置 vm.dirty_background_ratio 参数为 5
-echo "Setting vm.dirty_background_ratio to 5"
 sudo sysctl -w vm.dirty_background_ratio=5
-
-# 设置 vm.dirty_expire_centisecs 参数为 500
-echo "Setting vm.dirty_expire_centisecs to 500"
 sudo sysctl -w vm.dirty_expire_centisecs=500
-
-# 设置 vm.vfs_cache_pressure 参数为 200
-echo "Setting vm.vfs_cache_pressure to 200"
 sudo sysctl -w vm.vfs_cache_pressure=200
+
+# 重新加载 sysctl 配置文件以使更改生效
+echo "Reloading sysctl configuration"
+sudo sysctl -p
 
 echo "Changes applied successfully."
