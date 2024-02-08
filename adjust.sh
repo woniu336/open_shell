c459e87f0a6eb3f11dde3cf25b8511c8c3fc0705
woniu336 <310478073@qq.com>
2024-02-08 17:58:34 +0800

    Update adjust.sh

--- adjust.sh
+++ adjust.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# 清除之前的设置
-sudo sysctl -w vm.swappiness=0
-sudo sysctl -w vm.dirty_ratio=0
-sudo sysctl -w vm.dirty_background_ratio=0
-sudo sysctl -w vm.dirty_expire_centisecs=0
-sudo sysctl -w vm.vfs_cache_pressure=0
+# 定义参数数组
+declare -a parameters=(
+    "vm.swappiness=10"
+    "vm.dirty_ratio=10"
+    "vm.dirty_background_ratio=5"
+    "vm.dirty_expire_centisecs=500"
+    "vm.vfs_cache_pressure=200"
+)
 
-# 设置新的参数值
-sudo sysctl -w vm.swappiness=10
-sudo sysctl -w vm.dirty_ratio=10
-sudo sysctl -w vm.dirty_background_ratio=5
-sudo sysctl -w vm.dirty_expire_centisecs=500
-sudo sysctl -w vm.vfs_cache_pressure=200
+# 检查每个参数是否已存在，如果不存在则应用新参数
+for param in "${parameters[@]}"; do
+    current_setting=$(sudo sysctl -n "$param" 2>/dev/null)
+    if [[ -z "$current_setting" ]]; then
+        echo "Setting $param"
+        sudo sysctl -w "$param"
+    else
+        echo "Skipping $param as it is already set to $current_setting"
+    fi
+done
 
 # 重新加载 sysctl 配置文件以使更改生效
 echo "Reloading sysctl configuration"
