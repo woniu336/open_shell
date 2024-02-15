e9aa501025281fa8ca390f1c5b212038cf2ddee5
woniu336 <310478073@qq.com>
2024-02-15 19:32:11 +0800

    66

--- xitong.sh
+++ xitong.sh
@@ -63,8 +63,8 @@ echo "系统更新和软件安装完成。"
 
             if [ "$choice" == "y" ]; then
                 # 定义DNS地址
-                cloudflare_ipv4="1.0.0.1"
-                google_ipv4="8.8.8.8"
+                cloudflare_ipv4="223.5.5.5"
+                google_ipv4="119.29.29.29"
                 cloudflare_ipv6="2606:4700:4700::1111"
                 google_ipv6="2001:4860:4860::8888"
 
