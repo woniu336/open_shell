fe52b7b13fdf93025f8a17d42ee748988dcadcdf
woniu336 <310478073@qq.com>
2024-04-27 20:07:16 +0800

    Docs: 『note』内容更新 2024-04-27 Sat 20:07:16

--- fail2ban.sh
+++ fail2ban.sh
@@ -18,7 +18,7 @@ if [ -x "$(command -v fail2ban-client)" ] && [ -d "/etc/fail2ban" ]; then
         echo "------------------------"
         echo "11. 配置拦截参数"
         echo "------------------------"
-        echo "21. cloudflare模式"
+        echo "12. 对接cloudflare防火墙"
         echo "------------------------"
         echo "9. 卸载防御程序"
         echo "------------------------"
@@ -29,128 +29,104 @@ if [ -x "$(command -v fail2ban-client)" ] && [ -d "/etc/fail2ban" ]; then
         case $sub_choice in
             1)
                 sed -i 's/false/true/g' /etc/fail2ban/jail.d/sshd.local
-                systemctl restart fail2ban
-                service fail2ban restart
-                sleep 1
-                fail2ban-client status
-                continue
                 ;;
             2)
                 sed -i 's/true/false/g' /etc/fail2ban/jail.d/sshd.local
-                systemctl restart fail2ban
-                service fail2ban restart
-                sleep 1
-                fail2ban-client status
-                continue
                 ;;
             3)
                 sed -i 's/false/true/g' /etc/fail2ban/jail.d/nginx.local
-                systemctl restart fail2ban
-                service fail2ban restart
-                sleep 1
-                fail2ban-client status
-                continue
                 ;;
             4)
                 sed -i 's/true/false/g' /etc/fail2ban/jail.d/nginx.local
-                systemctl restart fail2ban
-                service fail2ban restart
-                sleep 1
-                fail2ban-client status
-                continue
                 ;;
             5)
-                echo "------------------------"
                 fail2ban-client status sshd
-                echo "------------------------"
-                continue
                 ;;
             6)
-                echo "------------------------"
                 fail2ban-client status fail2ban-nginx-cc
-                echo "------------------------"
                 fail2ban-client status nginx-bad-request
-                echo "------------------------"
                 fail2ban-client status nginx-botsearch
-                echo "------------------------"
                 fail2ban-client status nginx-http-auth
-                echo "------------------------"
                 fail2ban-client status nginx-limit-req
-                echo "------------------------"
                 fail2ban-client status php-url-fopen
-                echo "------------------------"
-                continue
                 ;;
             7)
                 fail2ban-client status
-                continue
                 ;;
             8)
                 tail -f /var/log/fail2ban.log
-                continue
                 ;;
             9)
                 systemctl disable fail2ban
-				systemctl stop fail2ban
-				apt remove -y --purge fail2ban
-				find / -name "fail2ban" -type d
-				rm -rf /etc/fail2ban
-                continue
+                systemctl stop fail2ban
+                apt remove -y --purge fail2ban
+                find / -name "fail2ban" -type d
+                rm -rf /etc/fail2ban
                 ;;
             11)
                 install nano
                 nano /etc/fail2ban/jail.d/nginx.local
-                systemctl restart fail2ban
-                service fail2ban restart
-                continue
                 ;;
-            21)
-                echo "到cf后台右上角我的个人资料，选择左侧API令牌，获取Global API Key"
-                echo "https://dash.cloudflare.com/login"
+            12)
+                echo -e "\e[32m到cf后台右上角我的个人资料，选择左侧API令牌，获取Global API Key\e[0m"
+                echo -e "\e[32m获取地址: https://dash.cloudflare.com/login\e[0m"
                 read -p "输入CF的账号: " cfuser
                 read -p "输入CF的Global API Key: " cftoken
 
-                wget -O /www/server/panel/vhost/nginx/default.conf https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/default.conf
+                echo -e "\e[33m脚本执行中...\e[0m"
+                wget -O /www/server/panel/vhost/nginx/default.conf https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/default.conf > /dev/null 2>&1
 				
 				mkdir -p /www/server/panel/vhost/cert/default
 				
                 cd /www/server/panel/vhost/cert/default/
-                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/default_server.crt
-				curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/default_server.key
+                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/default_server.crt > /dev/null 2>&1
+				curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/default_server.key > /dev/null 2>&1
 							
                 cd /etc/fail2ban/filter.d/
-                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/fail2ban-nginx-cc.conf
-				curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/nginx-bad-request.conf
+                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/fail2ban-nginx-cc.conf > /dev/null 2>&1
+				curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/nginx-bad-request.conf > /dev/null 2>&1
 				
 				rm -rf /etc/fail2ban/jail.d/*
 				
                 cd /etc/fail2ban/jail.d/
-                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/nginx.local
+                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/nginx.local > /dev/null 2>&1
 				
 			    cd /etc/fail2ban/jail.d/
-                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/sshd.local
+                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/sshd.local > /dev/null 2>&1
 
                 cd /etc/fail2ban/action.d/
-                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/cloudflare.conf
+                curl -sS -O https://raw.githubusercontent.com/woniu336/open_shell/main/fail2ban/cloudflare.conf > /dev/null 2>&1
 
                 sed -i "s/kejilion@outlook.com/$cfuser/g" /etc/fail2ban/action.d/cloudflare.conf
                 sed -i "s/APIKEY00000/$cftoken/g" /etc/fail2ban/action.d/cloudflare.conf
 
                 systemctl restart fail2ban
                 service fail2ban restart
-                docker restart nginx
+                service nginx restart
 
-                echo "已配置cloudflare模式，可在cf后台，站点-安全性-事件中查看拦截记录"
-                continue
+                echo -e "\e[32m已对接cloudflare防火墙，可在cf后台，站点-安全性-事件中查看拦截记录\e[0m"
+                
                 ;;
             0)
                 break
                 ;;
             *)
                 echo "无效的选择,请重试。"
-                continue
                 ;;
         esac
+
+        # 在某些选项执行后不返回主菜单
+        if [[ $sub_choice == 1 || $sub_choice == 2 || $sub_choice == 3 || $sub_choice == 4 ]]; then
+            systemctl restart fail2ban
+            service fail2ban restart
+            sleep 1
+            fail2ban-client status
+        elif [[ $sub_choice == 9 ]]; then
+            continue  # 如果选择卸载，则继续循环
+        fi
+
+        # 暂停等待用户输入
+        read -n 1 -s -r -p "按任意键继续..."
     done
 else
     echo "未检测到 fail2ban 服务,请先安装 fail2ban。"
