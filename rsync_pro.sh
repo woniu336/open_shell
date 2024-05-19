eb7c8704c512c627a4dbed8151139a34e5527a5d
woniu336 <310478073@qq.com>
2024-05-19 19:06:41 +0800

    Docs: 『note』内容更新 2024-05-19 Sun 19:06:41

--- rsync_pro.sh
+++ rsync_pro.sh
@@ -146,21 +146,27 @@ establish_ssh_connection() {
 test_ssh_connection() {
     echo -e "${huang}正在尝试连接到远程服务器...${bai}"
 
-    if ! ssh -p $SSH_PORT -i ~/.ssh/id_ed25519 -o "StrictHostKeyChecking=no" -o "BatchMode=yes" $REMOTE_USER@$REMOTE_HOST "exit"; then
+    # 使用 SSH 连接并显示进度条
+    echo -ne "连接中... "
+    if ! ssh -p $SSH_PORT -i ~/.ssh/id_ed25519 -o "StrictHostKeyChecking=no" -o "BatchMode=yes" $REMOTE_USER@$REMOTE_HOST "exit" 2>/dev/null; then
+        echo -e "失败\n"
         # 如果连接失败，尝试将公钥复制到远程服务器
-        echo -e "${huang}测试连接中...${bai}"
+        echo -ne "${huang}测试连接中...${bai}\n"
         if ssh-copy-id -i ~/.ssh/id_ed25519.pub -p $SSH_PORT $REMOTE_USER@$REMOTE_HOST; then
             echo -e "${lv}SSH 已成功连接到远程服务器。${bai}"
         else
-            echo -e "${hong}无法连接到远程服务器。请检查详细信息。${bai}"
+            echo -e "${hong}无法连接到远程服务器。请检查详细信息：${bai}"
+            echo -e "公钥复制失败。\n"
             exit 1
         fi
     else
-        echo -e "${lv}SSH 连接成功！${bai}"
+        echo -e "成功${bai}\n"
     fi
+
     read -n 1 -s -p "按任意键继续..."
     return_to_main_menu
 }
+
 
 
 # 同步文件
