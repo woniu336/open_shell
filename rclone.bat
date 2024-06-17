675a7696e14c56c1eb1ed4870074e65a5805017c
woniu336 <310478073@qq.com>
2024-06-17 16:20:57 +0800

    Docs: 『note』内容更新 2024-06-17 Mon 16:20:57

--- rclone.bat
+++ rclone.bat
@@ -13,11 +13,22 @@ echo ==============================================
 
 :: 获取用户输入
 echo.
-set /p source=请输入源路径： 
+set /p source=请输入源路径:
 echo.
-set /p destination=请输入目标路径： 
+set /p destination=请输入目标路径:
 echo.
-set /p proxy=是否需要代理？(y/n)：默认n 
+set /p proxy=是否需要代理？(y/n):
+if "%proxy%"=="" set proxy=n
+
+:: 提示是否忽略目标目录中已有的文件，默认忽略
+echo.
+set /p ignore_existing=是否忽略目标目录中已有的文件？(y/n):
+if "%ignore_existing%"=="" set ignore_existing=y
+
+:: 提示同时进行的文件传输数量，默认6
+echo.
+set /p transfers=请输入同时进行的文件传输数量(6-20):
+if "%transfers%"=="" set transfers=6
 
 :: 创建并写入新bat脚本内容
 (
@@ -30,7 +41,11 @@ if /i "%proxy%"=="y" (
     echo set https_proxy=
 )
 echo cd /d D:\rclone-v1.63.0-windows-amd64
-echo rclone copy "%source%" "%destination%" --ignore-existing -u -v -P --transfers=20 --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse
+if /i "%ignore_existing%"=="y" (
+    echo rclone copy "%source%" "%destination%" --ignore-existing -u -v -P --transfers=%transfers% --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse
+) else (
+    echo rclone copy "%source%" "%destination%" -u -v -P --transfers=%transfers% --ignore-errors --buffer-size=128M --check-first --checkers=10 --drive-acknowledge-abuse
+)
 echo pause
 ) > work001.bat
 
